77f0917c1572a548bd96fdcfa89a5ba44858fdea
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2015-02-05 18:37:59 +0530

    Add dummy SUB record in CDMA NV mode
    Add dummy SUB info record when NV is ready in CDMA NV mode.
    
    CRs-Fixed: 789989
    
    Change-Id: Ie658087efaa9f93a329773d2d8f0601d083f3701

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -595,4 +595,8 @@ public class PhoneFactory {
             ipw.flush();
         }
     }
+
+    public static SubscriptionInfoUpdater getSubscriptionInfoUpdater() {
+        return sSubInfoRecordUpdater;
+    }
 }
